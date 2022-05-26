7b82a5e120e0319daf45f857861cfd68dc996406
tsamu2000 <78439186+tsamu2000@users.noreply.github.com>
2022-05-27 00:08:40 +0200

    Third commit

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
@@ -7,8 +11,14 @@ import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
 import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.stext.stext.EventDefinition;
+import org.yakindu.sct.model.stext.stext.VariableDefinition;
 
 import hu.bme.mit.model2gml.Model2GML;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
+import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
 
 public class Main {
@@ -50,11 +60,51 @@ public class Main {
 					System.out.println("Nincs neve az allapotnak. Javaslat: Allapot"+j+".");
 				}
 			}
+			
+			if (content instanceof EventDefinition) 
+			{
+				EventDefinition event = (EventDefinition) content;
+				System.out.println("Event: "+event.getName()+".");
+			}
+			
+			if (content instanceof VariableDefinition) 
+			{
+				VariableDefinition variable = (VariableDefinition) content;
+				System.out.println("Variable: "+variable.getName()+".");
+			}
 		}
+		
+		ExampleStatemachine e = new ExampleStatemachine();
+		e.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(e, 200);
+		e.init();
+		e.enter();
+		e.runCycle();
+		
+		printFunc(e);
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
 		// and saving it
 		manager.saveFile("model_output/graph.gml", content);
+	}
+	
+	public static void printFunc(IExampleStatemachine e) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		try {
+			line = br.readLine();
+			while (!line.equals("exit")) 
+			{
+				System.out.println("public static void print(IExampleStatemachine s) {");
+				System.out.println("System.out.println(\" "+line.substring(0, 1).toUpperCase()+" = \" + s.getSCInterface().get"
+				+line.substring(0, 1).toUpperCase()+line.substring(1)+"());");
+				System.out.println("}");
+				line = br.readLine();
+			}
+			br.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 }
